0c48acacbbf58c7e82d64d489f04bb34c21d85bd
Sujith P <sujithp2690@gmail.com>
2023-04-12 11:38:20 +0530

    Host Backend

--- src/API/baseUrl.js
+++ src/API/baseUrl.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const baseUrl = axios.create({baseURL:process.env.BASE_URL})
+export const baseUrl = axios.create({baseURL: 'https://students-bp7z.onrender.com' })
